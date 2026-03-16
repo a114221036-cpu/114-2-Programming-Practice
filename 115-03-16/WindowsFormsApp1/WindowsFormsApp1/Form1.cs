045d36c392c2e3a53f530b189149651dd185c8aa
a114221036-cpu <a114221036@mail.shu.edu.tw>
2026-03-16 18:52:38 +0800

    115-03-16

---
+++ 115-03-16/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PassValue(int x, int y)
+        {
+            LblMsg.Text += $"2.方法中:參數計算前: x = {x}, y = {y}\n\n";
+            x += 3; //參數 x 加 3
+            y += 2; //參數 y 加 2
+            LblMsg.Text += $"3.方法中:參數計算後: x = {x}, y = {y}\n\n";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = 10, b = 15;
+            LblMsg.Text = $"1.主程式:呼叫方法前: a = {a}, b = {b}\n\n";
+            PassValue(a, b);
+            LblMsg.Text += $"4.主程式:呼叫方法後: a = {a}, b = {b}\n\n";
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            
+            int a = 10, b = 15;
+            LblMsg.Text = $"1.主程式:呼叫方法前: a = {a}, b = {b}\n\n";
+            PassValue(a, b);
+            LblMsg.Text += $"4.主程式:呼叫方法後: a = {a}, b = {b}\n\n";
+        }
+
+        private void LblMsg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 115-03-16/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void PassRef(ref int x, ref int y)
+        {
+            LblMsg.Text += $"2.方法中:參數計算前: x = {x}, y = {y}\n\n";
+            x += 3; //參數 x 加 3
+            y += 2; //參數 y 加 2
+            LblMsg.Text += $"3.方法中:參數計算後: x = {x}, y = {y}\n\n";
+        }
+        
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            int a = 10, b = 15;
+            LblMsg.Text = $"1.主程式:呼叫方法前: a = {a}, b = {b}\n\n";
+            PassRef(ref a, ref b);
+            LblMsg.Text += $"4.主程式:呼叫方法後: a = {a}, b = {b}\n\n";
+        }
+    }
+}
