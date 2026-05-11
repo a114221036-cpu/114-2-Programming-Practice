fec788853920a66c81ebd6fef13254805941f4de
a114221036-cpu <a114221036@mail.shu.edu.tw>
2026-05-11 18:56:06 +0800

    115-05-11

---
+++ 115-05-11/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student s1 = new Student();
+            s1.Name = " 珍妮佛";
+            s1.Score = 5000;        //設定Jennifer的Score值為5000
+            s1.ShowMsg();
+            Student s2 = new Student();
+            s2.Name = "布魯斯";
+            s2.Score = -100;          //設定Bruce的Score值為-100
+            s2.ShowMsg();
+        }
+    }
+}
---
+++ 115-05-11/WindowsFormsApp1/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Student
+    {
+        private string name; //name姓名欄位宣告為private
+        private int _score;  //_score姓名欄位宣告為private
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Score   //建立Score屬性，此屬性限制在0-100
+            {
+            get { 
+                  return _score; 
+                }
+            set
+            {
+                if (value >= 100) value = 100;
+                if (value <= 0) value = 0;
+                _score = value;
+            
+            }
+
+        }
+        public void ShowMsg()
+        {
+            MessageBox.Show($" {Name}同學的分數是 {Score}");
+        }
+    }
+}
